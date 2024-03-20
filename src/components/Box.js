c1ca97bc312e731e92c100720fb0317bcea6a6f0
sunnyheee <fool0116@naver.com>
2024-03-20 12:30:39 +0900

    2. 첫번째 프로젝트: 가위바위보 게임 5강~6강

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,11 +1,22 @@
 import React from "react";
 
 const Box = (props) => {
+  const boxClass =
+    props.result === "win" ? "win" : props.result === "lose" ? "lose" : "";
+
   return (
-    <div className="box">
+    <div className={`box ${boxClass}`}>
       <h1>{props.title}</h1>
-      <img className="box-img" src={props.item && props.item.img} alt="rock" />
-      <p>Win</p>
+      {props.item && props.item.img ? (
+        <img
+          className="box-img"
+          src={props.item && props.item.img}
+          alt="rock"
+        />
+      ) : (
+        <p className="noitemtxt">가위바위보를 선택해 주세요</p>
+      )}
+      <p className="result">{props.result}</p>
     </div>
   );
 };
